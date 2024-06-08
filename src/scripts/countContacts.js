c5393c207c12ee4935b018c5ed07e1af0b029c39
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-08 20:42:10 +0300

    add countContacts.js

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,20 @@
-// import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { PATH_DB } from '../constants/contacts.js';
 
-// export const countContacts = async () => {};
 
-// console.log(await countContacts());
+export const countContacts = async () => {
+  try {
+    // Read the content of the db.json file
+    const data = await fs.readFile(PATH_DB, 'utf8');
+    // Parse the JSON data to a JavaScript array
+    const contacts = JSON.parse(data);
+    // Return the number of contacts
+    return contacts.length;
+  } catch (error) {
+    console.error('Error counting contacts:', error);
+    return 0;
+  }
+};
+
+// Log the result of countContacts function
+console.log(await countContacts());
