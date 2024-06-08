639db368ab19157bdf490b39442d148d3cf81277
keraberas <46618649+keraberase@users.noreply.github.com>
2024-06-08 20:51:57 +0300

    fix proplem

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,8 @@
-export const PATH_DB = './src/db/db.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get the current file path in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const PATH_DB = path.join(__dirname, '../db/db.json');
